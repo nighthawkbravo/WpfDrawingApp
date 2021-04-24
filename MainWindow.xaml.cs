11b7a21223faed470c57b404419e919af62cce2b
nighthawkbravo <lucas@aggati.com>
2021-04-24 12:30:44 +0200

    Began implementing various functions

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using WpfAppComputerGraphics2.Shapes;
 using Color = System.Drawing.Color;
 using Point = System.Drawing.Point;
 
@@ -23,23 +23,134 @@ namespace WpfAppComputerGraphics2
     /// </summary>
     public partial class MainWindow : Window
     {
-        // A blank BitmapImage.
-        private readonly BitmapImage blankBMI;
-
         // The BitmapImage that is being drawn on.
-        private BitmapImage curBMI;
+        private BitmapImage bmi;
+
+        private List<IShape> layers;
+
+        private bool lineClickFlag = false;
+        private int lineClickNum = 0;
+
+        private bool circleClickFlag = false;
+        private bool polyClickFlag = false;
+        private List<Point> CanvasPoints;
+
+        private int LineThickValue;
+        private int PolyThickValue;
+
+        private BrushConverter bc = new BrushConverter();
+        private string mylightRed = "#FF5555";
+        private string mylightGreen = "#42f548";
 
         public MainWindow()
         {
             InitializeComponent();
+            layers = new List<IShape>();
+            CanvasPoints = new List<Point>();
         }
-        
 
+        private void LineThickChange(object sender, TextChangedEventArgs e)
+        {
+            int thick=0;
+            if(Int32.TryParse(LineThickBox.Text, out thick) && thick % 2 != 0)
+            {
+                LineThickBox.Background = (Brush)bc.ConvertFrom(mylightGreen);
+                LineThickValue = thick;
+            }
+            else
+            {
+                LineThickBox.Background = (Brush)bc.ConvertFrom(mylightRed);
+            }
+        }
+        private void PolyThickChange(object sender, TextChangedEventArgs e)
+        {
+            int thick = 0;
+            if (Int32.TryParse(LineThickBox.Text, out thick) && thick % 2 != 0)
+            {
+                PolyThickBox.Background = (Brush)bc.ConvertFrom(mylightGreen);
+                PolyThickValue = thick;
+            }
+            else
+            {
+                PolyThickBox.Background = (Brush)bc.ConvertFrom(mylightRed);
+            }
+        }
         private void ImageMouseMove(object sender, MouseEventArgs e)
         {
             var p = e.GetPosition(myImage);
             MouseXText.Text = $"X: {p.X:0.}";
             MouseYText.Text = $"Y: {p.Y:0.}";
+        }
+        private void ImageMouseLeave(object sender, MouseEventArgs e)
+        {
+            MouseXText.Text = $"X: -";
+            MouseYText.Text = $"Y: -";
+        }
+        private void RenderLayers()
+        {
+            foreach(var shape in layers)
+            {
+                bmi = shape.Render(bmi);
+            }
+            DisplayImageFromBMI(bmi);
+            //MessageBox.Show("Rendering");
+        }
+        private void DisplayImageFromBMI(BitmapImage bmi)
+        {
+            myImage.Source = bmi;
+        }
+        private void ResetFlagsAndCP()
+        {
+            CanvasPoints.Clear();
+
+            lineClickFlag = false;
+            circleClickFlag = false;
+            polyClickFlag = false;
+        }
+
+
+        private void ImageMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (lineClickFlag)
+            {
+                int x = (int)e.GetPosition(myImage).X;
+                int y = (int)e.GetPosition(myImage).Y;
+                CanvasPoints.Add(new Point(x, y));
+                lineClickNum--;
+                if (lineClickNum <= 0)
+                {
+                    layers.Add(new Line(CanvasPoints[0], CanvasPoints[1]));
+                    //MessageBox.Show($"{CanvasPoints[0]} - {CanvasPoints[1]}");
+                    CanvasPoints.Clear();
+                    lineClickFlag = false;
+                    RenderLayers();
+                }
+            } // Line Handling
+        }
+
+        // ----- Menu Buttons -----
+
+        // ----- Line -----
+        private void DrawLineButton(object sender, RoutedEventArgs e)
+        {
+            if (!lineClickFlag)
+            {
+                ResetFlagsAndCP();
+                lineClickFlag = true;
+                lineClickNum = 2;                
+            }
+        }
+
+
+
+
+
+
+
+        private void ClearCanvasButton(object sender, RoutedEventArgs e)
+        {
+            layers.Clear();
+            DisplayImageFromBMI(null);
         }
     }
 }
