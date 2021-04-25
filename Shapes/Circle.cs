8dd4d70cc1e4d65dd5aaff2e2d985632145142fc
nighthawkbravo <lucas@aggati.com>
2021-04-25 19:14:12 +0200

    Added Circle drawing, moving, and radius change

--- Shapes/Circle.cs
+++ Shapes/Circle.cs
@@ -53,6 +53,90 @@ namespace WpfAppComputerGraphics2.Shapes
             }
             return false;
         }
-        public Bitmap Render(Bitmap bm) { return bm; }
+        private double CalcEucliDist(Point p1, Point p2)
+        {
+            return Math.Sqrt(Math.Pow(p2.Y - p1.Y, 2) + Math.Pow(p2.X - p1.X, 2));
+        }
+        public Bitmap Render(Bitmap bm)
+        {
+            int r = (int)CalcEucliDist(Center, EdgePoint);
+
+            bm = midPointCircleDraw(Center.X, Center.Y, r, bm);
+
+            return bm;
+        }
+
+        private Bitmap midPointCircleDraw(int x_centre, int y_centre, int r, Bitmap bm)
+        {
+            int x = r, y = 0;
+
+            
+            // When radius is zero only a single
+            // point will be printed
+            if (r > 0)
+            {
+                //Console.Write("(" + (x + x_centre) + ", " + (-y + y_centre) + ")");
+                ColorPixel(x + x_centre, -y + y_centre, bm);
+                //Console.Write("(" + (y + x_centre) + ", " + (x + y_centre) + ")");
+                ColorPixel(y + x_centre, x + y_centre, bm);
+                //Console.WriteLine("(" + (-y + x_centre) + ", " + (x + y_centre) + ")");
+                ColorPixel(-y + x_centre, x + y_centre, bm);
+            }
+
+            // Initialising the value of P
+            int P = 1 - r;
+            while (x > y)
+            {
+                y++;
+
+                // Mid-point is inside or on the perimeter
+                if (P <= 0) P = P + 2 * y + 1;
+
+                // Mid-point is outside the perimeter
+                else
+                {
+                    x--;
+                    P = P + 2 * y - 2 * x + 1;
+                }
+
+                // All the perimeter points have already 
+                // been printed
+                if (x < y) break;
+
+                // Printing the generated point and its 
+                // reflection in the other octants after
+                // translation
+
+                //Console.Write("(" + (x + x_centre) + ", " + (y + y_centre) + ")");
+                ColorPixel(x + x_centre, y + y_centre, bm);
+                //Console.Write("(" + (-x + x_centre) + ", " + (y + y_centre) + ")");
+                ColorPixel(-x + x_centre, y + y_centre, bm);
+                //Console.Write("(" + (x + x_centre) + ", " + (-y + y_centre) + ")");
+                ColorPixel(x + x_centre, -y + y_centre, bm);
+                //Console.WriteLine("(" + (-x + x_centre) + ", " + (-y + y_centre) + ")");
+                ColorPixel(-x + x_centre, -y + y_centre, bm);
+
+                // If the generated point is on the 
+                // line x = y then the perimeter points
+                // have already been printed
+                if (x != y)
+                {
+                    //Console.Write("(" + (y + x_centre) + ", " + (x + y_centre) + ")");
+                    ColorPixel(y + x_centre, x + y_centre, bm);
+                    //Console.Write("(" + (-y + x_centre) + ", " + (x + y_centre) + ")");
+                    ColorPixel(-y + x_centre, x + y_centre, bm);
+                    //Console.Write("(" + (y + x_centre) + ", " + (-x + y_centre) + ")");
+                    ColorPixel(y + x_centre, -x + y_centre, bm);
+                    //Console.WriteLine("(" + (-y + x_centre) + ", " + (-x + y_centre) + ")");
+                    ColorPixel(-y + x_centre, -x + y_centre, bm);
+                }
+            }
+            return bm;
+        }
+        public void ColorPixel(int x, int y, Bitmap bm)
+        {
+            if (!IsInBound(x, y, bm)) return;
+            bm.SetPixel(x, y, myColor);
+        }
     }
 }
